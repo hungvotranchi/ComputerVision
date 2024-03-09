93970734e144e8281d2c9f2267a10ae98a755926
Cy30mk3v <40456349+Cy30mk3v@users.noreply.github.com>
2024-03-10 00:14:39 +0700

    Update download scripts

---
+++ ImageClassification/model/AlexNet.py
@@ -0,0 +1,46 @@
+import torch
+import torchvision
+import torch.nn as nn
+
+
+class AlexNet(nn.Module):
+    def __init__(self, num_class):
+        super().__init__()
+        self.blockCNN1 = nn.Sequential(
+            nn.Conv2d(in_channels=3,out_channels=96, kernel_size= 11, stride= 4),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size= 3, stride= 2)
+        )
+
+        self.blockCNN2 = nn.Sequential(
+            nn.Conv2d(in_channels=96, out_channels= 256, kernel_size= 5, stride= 1, padding= 2),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size= 3, stride= 2)
+        )
+        self.blockCNN3 = nn.Sequential(
+            nn.Conv2d(in_channels=256, out_channels= 384, kernel_size= 3, stride= 1, padding= 1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=384, out_channels= 384, kernel_size= 3, stride= 1, padding= 1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=384, out_channels= 256, kernel_size= 3, stride= 1, padding= 1),
+            nn.MaxPool2d(kernel_size= 3, stride= 2)
+        )
+
+        self.classifier = nn.Sequential(
+            nn.Linear(in_features= 256 * 3 * 3, out_features= 4096),
+            nn.ReLU(),
+            nn.Dropout(),
+            nn.Linear(in_features = 4096, out_features=4096),
+            nn.ReLU(),
+            nn.Dropout(),
+            nn.Linear(in_features= 4096, out_features= num_class)
+        )
+
+    
+    def forward(self, X):
+        X = self.blockCNN1(X)
+        X = self.blockCNN2(X)
+        X = self.blockCNN3(X)
+        X = self.classifier(torch.flatten(X, 1))
+        return X
+    
--- ImageClassification/scripts/download.sh
+++ ImageClassification/scripts/download.sh
@@ -1,4 +1,9 @@
-wget -nc https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz
-mkdir datasets/CIFAR10
-tar -zxvf cifar-10-python.tar.gz --directory datasets/CIFAR10 -k 
-rm cifar-10-python.tar.gz
+# wget -nc https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz
+# mkdir datasets/CIFAR10
+# tar -zxvf cifar-10-python.tar.gz --directory datasets/CIFAR10 -k 
+# rm cifar-10-python.tar.gz
+
+wget -nc https://www.robots.ox.ac.uk/~vgg/data/fgvc-aircraft/archives/fgvc-aircraft-2013b.tar.gz
+mkdir datasets/fgvc
+tar -zxvf fgvc-aircraft-2013b.tar.gz --directory datasets/fgvc -k 
+rm fgvc-aircraft-2013b.tar.gz
