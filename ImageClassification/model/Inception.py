bc88ad677c45a3359c573c50ad624a00d6c026c9
Cy30mk3v <40456349+Cy30mk3v@users.noreply.github.com>
2024-03-11 23:31:59 +0700

    Update  Inception architecture

--- ImageClassification/model/Inception.py
+++ ImageClassification/model/Inception.py
@@ -36,7 +36,7 @@ class InceptionBlock(nn.Module):
         )
 
         self.branch_4 = nn.Sequential(
-            nn.MaxPool2d(kernel_size= 3, stride= 1, padding= 1),
+            nn.MaxPool2d(kernel_size= 3, stride= 2, padding= 1),
             nn.Conv2d(in_channels=in_channels, out_channels= pool_prj, kernel_size=1),
             nn.BatchNorm2d(pool_prj),
             nn.ReLU(True)
@@ -61,19 +61,19 @@ class InceptionNet(nn.Module):
 
         self.a3 = InceptionBlock(192, 64, 96, 128, 16, 32, 32)
         self.b3 = InceptionBlock(256, 128, 128, 192, 32, 96, 64)
-        self.maxpool = nn.MaxPool2d(kernel_size= 3, stride= 2, padding= 1),
+        self.maxpool = nn.MaxPool2d(kernel_size= 3, stride= 2, padding= 1)
 
-        self.a4 = InceptionBlock(480, 192, 96, 208, 16, 48, 64),
-        self.b4 = InceptionBlock(512, 160, 112, 224, 24, 64, 64),
-        self.c4 = InceptionBlock(512, 128, 128, 256, 24, 64, 64),
-        self.d4 = InceptionBlock(512, 112, 144, 288, 32, 64, 64),
-        self.e4 = InceptionBlock(528, 256, 160, 320, 32, 128, 128),
+        self.a4 = InceptionBlock(480, 192, 96, 208, 16, 48, 64)
+        self.b4 = InceptionBlock(512, 160, 112, 224, 24, 64, 64)
+        self.c4 = InceptionBlock(512, 128, 128, 256, 24, 64, 64)
+        self.d4 = InceptionBlock(512, 112, 144, 288, 32, 64, 64)
+        self.e4 = InceptionBlock(528, 256, 160, 320, 32, 128, 128)
 
-        self.a5 = InceptionBlock(832, 256, 160, 320, 32, 128, 128),
-        self.b5 = InceptionBlock(832, 384, 192, 384, 48, 128, 128),
+        self.a5 = InceptionBlock(832, 256, 160, 320, 32, 128, 128)
+        self.b5 = InceptionBlock(832, 384, 192, 384, 48, 128, 128)
 
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-        self.dropout = nn.Dropout(0.5),
+        self.dropout = nn.Dropout(0.5)
         self.fc = nn.Linear(in_features= 1024, out_features= num_classes)
 
 
