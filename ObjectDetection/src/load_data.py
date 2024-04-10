1e446a28fa110538ce88b92c0262198348ca5b40
hungvotranchi <hung.tran.vo@deltacognition.com>
2024-04-10 23:52:56 +0700

    Update loading file in Object Detection

--- ObjectDetection/src/load_data.py
+++ ObjectDetection/src/load_data.py
@@ -9,17 +9,9 @@ transform_sub = transforms.Compose(
     [transforms.ToTensor(),
      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-def load_COCO(transform):
-    data_path = f"..\datasets\COCO"
-    data_path_linux = f"../datasets/COCO"
-    try:
-        coco = datasets.CocoDetection(root=f"{data_path}/train2017/", annFile = f"{data_path}/annotations/instances_train2017.json", \
+def load_COCO(transform, data_path):
+    coco = datasets.CocoDetection(root=f"{data_path}/train2017/", annFile = f"{data_path}/annotations/instances_train2017.json", \
                                 download= False, transform= transform)
-        coco_val = datasets.CocoDetection(root=f"{data_path}/val2017/", annFile = f"{data_path}/annotations/instances_val2017.json", \
-                                download= False, transform= transform)
-    except:
-        coco = datasets.CocoDetection(root=f"{data_path_linux}/train2017/", annFile = f"{data_path}/annotations/instances_train2017.json", \
-                                download= False, transform= transform)
-        coco_val = datasets.CocoDetection(root=f"{data_path_linux}/val2017/", annFile = f"{data_path}/annotations/instances_val2017.json", \
+    coco_val = datasets.CocoDetection(root=f"{data_path}/val2017/", annFile = f"{data_path}/annotations/instances_val2017.json", \
                                 download= False, transform= transform)
     return coco_val
