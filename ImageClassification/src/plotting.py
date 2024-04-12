eb5ff63f26bc7e62861cbd3455892e37e11a0dc0
hungvotranchi <hung.tran.vo@deltacognition.com>
2024-04-12 15:16:18 +0700

    Update src folder for ObjDetection

--- ImageClassification/src/plotting.py
+++ ImageClassification/src/plotting.py
@@ -1,8 +1,33 @@
-import matplotlib.pyplot as plt
-import numpy as np
+import PIL.Image
+from preprocess import *
 
-def imshow(img):
-    img = img / 2 + 0.5     # unnormalize
-    npimg = img.numpy()
-    plt.imshow(np.transpose(npimg, (1, 2, 0)))
-    plt.show()
+def show(sample):
+    import matplotlib.pyplot as plt
+
+    from torchvision.transforms.v2 import functional as F
+    from torchvision.utils import draw_bounding_boxes
+    
+    resize = Resize((300, 300))
+    
+    rhf = RandomHorizontalFlip()
+    rvf = RandomVerticalFlip()
+    image, target = sample
+    
+    image, bboxes = image,target["boxes"] 
+    
+    image, bboxes = resize(image, bboxes)
+    image, bboxes = rhf(image, bboxes)
+    image, bboxes = rvf(image, bboxes)
+    
+    if isinstance(image, PIL.Image.Image):
+        image = F.to_image_tensor(image)
+        
+    image = F.convert_dtype(image, torch.uint8)
+    annotated_image = draw_bounding_boxes(image, bboxes, colors="yellow", width=3)
+
+    fig, ax = plt.subplots()
+    ax.imshow(annotated_image.permute(1, 2, 0).numpy())
+    ax.set(xticklabels=[], yticklabels=[], xticks=[], yticks=[])
+    fig.tight_layout()
+
+    fig.show()
