8d4bb045047cc469ec7e9cd89be7b94342352c15
hungvotranchi <hung.tran.vo@deltacognition.com>
2024-04-12 15:36:01 +0700

    Update src

--- ObjectDetection/src/preprocess.py
+++ ObjectDetection/src/preprocess.py
@@ -1,43 +1,6 @@
 import torch
 from torchvision.transforms import transforms
 
-class RandomHorizontalFlip(object):
-    def __init__(self, p=0.5):
-        self.p = p
-        self.hf = transforms.RandomHorizontalFlip(1)
-        
-    def __call__(self, img, bboxes):
-        
-        if torch.rand(1)[0] < self.p:            
-            img = self.hf.forward(img)
-            bboxes = self.hf.forward(bboxes)
-        
-        return img, bboxes
-    
-    
-class RandomVerticalFlip(object):
-    def __init__(self, p=0.5):
-        self.p = p
-        self.vf = transforms.RandomVerticalFlip(1)
-        
-    def __call__(self, img, bboxes):
-        if torch.rand(1)[0] < self.p:                    
-            img = self.vf.forward(img)
-            bboxes = self.vf.forward(bboxes)
-        
-        return img, bboxes
-
-class Resize(object):
-    def __init__(self, size):
-        self.size = size
-        self.resize = transforms.Resize(self.size, antialias=True)
-        
-    def __call__(self, img, bboxes):
-        img = self.resize.forward(img)
-        
-        bboxes = self.resize.forward(bboxes)
-
-        return img, bboxes
     
 class CustomBatchs:
     def __init__(self, data):
