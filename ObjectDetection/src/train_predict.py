cc61ac6bf3bb7e8860a21b3c00151d6b75bf107d
hungvotranchi <hung.tran.vo@deltacognition.com>
2024-04-15 23:09:20 +0700

    Update tqdm for loss show

--- ObjectDetection/src/train_predict.py
+++ ObjectDetection/src/train_predict.py
@@ -18,47 +18,47 @@ def train(num_loops: int, optim: optim, model: nn.Module, \
         
         counter = 0
         model.train()
-    
-        for data_point in tqdm(train_dataloader, position=0, leave= True):
-            _i, _t = data_point[0], data_point[1]
-            
-            if device != "cuda":
-                _i = torch.stack(_i)
+        with tqdm(train_dataloader, position=0, leave= True) as tepoch:
+            for data_point in tepoch:
+                _i, _t = data_point[0], data_point[1]
+                
+                if device != "cuda":
+                    _i = torch.stack(_i)
 
-    #         _t = torch.from_numpy(np.asarray(_t))
-            
-            _i = _i.to(device)
-            _t = [{k: v.to(device) for k, v in __t.items()} for __t in _t]
+        #         _t = torch.from_numpy(np.asarray(_t))
+                
+                _i = _i.to(device)
+                _t = [{k: v.to(device) for k, v in __t.items()} for __t in _t]
 
-            optim.zero_grad()
+                optim.zero_grad()
 
 
-            loss_dict = model(_i, _t)
-            
-    #         running_bbox_loss += torch.mean(loss_dict['bbox_regression']).item()
-    #         running_classifier_loss += torch.mean(loss_dict['classification']).item()
+                loss_dict = model(_i, _t)
+                
+        #         running_bbox_loss += torch.mean(loss_dict['bbox_regression']).item()
+        #         running_classifier_loss += torch.mean(loss_dict['classification']).item()
 
-            losses = sum(loss for loss in loss_dict.values())
-        
-            losses.backward()
-            optim.step()
+                losses = sum(loss for loss in loss_dict.values())
             
-            running_loss += losses.item()
-            
-            del loss_dict, losses
-            
-            counter += 1
-            
-            if counter % 500 == 499:
-                last_classifier_loss = running_classifier_loss / 500 # loss per batch
-                last_bbox_loss = running_bbox_loss / 500 # loss per batch
-                last_loss = running_loss / 500 # loss per batch
-    #             print(f'batch {counter + 1} Classification Loss: {last_classifier_loss}', end='')
-    #             print(f', BBox Loss: {last_bbox_loss}')
-                print(f'Epoch {epoch}, Batch {counter + 1}, Running Loss: {last_loss}')
-                running_classifier_loss = 0.0
-                running_bbox_loss = 0.0
-                running_loss = 0.0
+                losses.backward()
+                optim.step()
+                
+                running_loss += losses.item()
+                
+                del loss_dict, losses
+                
+                counter += 1
+                
+                if counter % 500 == 499:
+                    last_classifier_loss = running_classifier_loss / 500 # loss per batch
+                    last_bbox_loss = running_bbox_loss / 500 # loss per batch
+                    last_loss = running_loss / 500 # loss per batch
+        #             print(f'batch {counter + 1} Classification Loss: {last_classifier_loss}', end='')
+        #             print(f', BBox Loss: {last_bbox_loss}')
+                    tepoch.set_postfix(loss = last_loss)
+                    running_classifier_loss = 0.0
+                    running_bbox_loss = 0.0
+                    running_loss = 0.0
             
         gc.collect()
 
