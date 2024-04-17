cb5ba14dbdc932ad2fb2d7b84f76b29cc237c5a6
hungvotranchi <hung.tran.vo@deltacognition.com>
2024-04-17 20:35:02 +0700

    Update engine

--- ObjectDetection/src/engine.py
+++ ObjectDetection/src/engine.py
@@ -9,6 +9,7 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 import utils
 from coco_eval import CocoEvaluator
 from coco_utils import get_coco_api_from_dataset
+import gc
 
 
 def train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq, scaler=None):
@@ -59,6 +60,9 @@ def train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq, sc
         metric_logger.update(loss=losses_reduced, **loss_dict_reduced)
         metric_logger.update(lr=optimizer.param_groups[0]["lr"])
 
+        del images, targets
+        gc.collect()
+    gc.collect()
     return metric_logger
 
 
@@ -105,6 +109,8 @@ def evaluate(model, data_loader, device):
         evaluator_time = time.time() - evaluator_time
         metric_logger.update(model_time=model_time, evaluator_time=evaluator_time)
 
+        del images, targets
+        gc.collect()
     # gather the stats from all processes
     metric_logger.synchronize_between_processes()
     print("Averaged stats:", metric_logger)
@@ -114,4 +120,5 @@ def evaluate(model, data_loader, device):
     coco_evaluator.accumulate()
     coco_evaluator.summarize()
     torch.set_num_threads(n_threads)
+    gc.collect()
     return coco_evaluator
