bb5272eb59388e0833f77e5c96ac5412d81f1f64
hungvotranchi <hung.tran.vo@deltacognition.com>
2024-04-20 23:19:33 +0700

    Update utils

--- ObjectDetection/src/coco_eval.py
+++ ObjectDetection/src/coco_eval.py
@@ -7,7 +7,7 @@ import pycocotools.mask as mask_util
 import torch
 import os
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
-import utils
+import src.utils_sub as utils_sub
 from pycocotools.coco import COCO
 from pycocotools.cocoeval import COCOeval
 
@@ -157,8 +157,8 @@ def convert_to_xywh(boxes):
 
 
 def merge(img_ids, eval_imgs):
-    all_img_ids = utils.all_gather(img_ids)
-    all_eval_imgs = utils.all_gather(eval_imgs)
+    all_img_ids = utils_sub.all_gather(img_ids)
+    all_eval_imgs = utils_sub.all_gather(eval_imgs)
 
     merged_img_ids = []
     for p in all_img_ids:
